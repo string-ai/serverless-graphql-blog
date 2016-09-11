e6d921357a762b47ec7db2de8d37f4a22a96fc1f
Rafael Nami <rafanami@gmail.com>
2016-09-11 20:42:32 +1200

    changes to make it work

---
+++ handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Your first function handler
+// module.exports.hello = (event, context, cb) => cb(null,
+//   { message: 'Go Serverless v1.0! Your function executed successfully!', event }
+// );
+
+// You can add more handlers here, and reference them in serverless.yml
+import graphql from './blog/graphql/handler';
+
+export default { graphql };
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './handler.js',
   target: 'node',
   externals: ["aws-sdk"],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: __dirname
+      include: __dirname,
+      exclude: /\.serverless/
     }]
   }
 };
