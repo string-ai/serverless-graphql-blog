43dac38d57e92eeeea0357f2b1a1550133d191f7
Rafael Nami <rafanami@gmail.com>
2016-09-11 20:41:50 +1200

    adding gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rn1466 on 3/09/16.
- */
-"use strict";
-
-import graphql from './blog/graphql/handler';
-
-export default { graphql };
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: __dirname,
-      exclude: /\aws-sdk/
+      include: __dirname
     }]
   }
 };
