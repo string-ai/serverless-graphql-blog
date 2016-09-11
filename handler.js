4a5f72220cf4fe4ab26b99658348442d5236f6e4
Rafael Nami <rafanami@gmail.com>
2016-09-12 00:12:35 +1200

    still deploying correctly but graphql not working as expected

--- handler.js
+++ handler.js
@@ -6,17 +6,6 @@
 // );
 
 // You can add more handlers here, and reference them in serverless.yml
-// import graphql from './blog/graphql/handler';
-//
-// export default { graphql };
-'use strict';
-
-/**
- * Serverless Module: Lambda Handler
- * - Your lambda functions should be a thin wrapper around your own separate
- * modules, to keep your code testable, reusable and AWS independent
- * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
- */
 // Require Serverless ENV vars
 var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
 
@@ -25,6 +14,7 @@ var lib = require('./blog/lib');
 
 // Lambda Handler
 module.exports.graphql = (event, context) => {
+
     lib.runGraphQL(event, (error, response) => {
         return context.done(error, response);
     });
