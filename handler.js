7382d5c6de114256861c858f69eb18d50756d8ce
Rafael Nami <rafanami@gmail.com>
2016-09-14 19:30:48 +1200

    Adding the working graphql server

--- handler.js
+++ handler.js
@@ -1,21 +1,15 @@
 'use strict';
 
-// Your first function handler
-// module.exports.hello = (event, context, cb) => cb(null,
-//   { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-// );
-
 // You can add more handlers here, and reference them in serverless.yml
 // Require Serverless ENV vars
 var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
 
 // Require Logic
-var lib = require('./blog/lib');
+var gql = require('./lib');
 
 // Lambda Handler
-module.exports.graphql = (event, context) => {
-
-    lib.runGraphQL(event, (error, response) => {
-        return context.done(error, response);
-    });
+export const graphql = (event, context, cb) => {
+    return gql(event.body)
+        .then((response) => cb(null, response))
+        .catch((error) => cb(error));
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ module.exports = {
   entry: './handler.js',
   target: 'node',
   externals: ["aws-sdk"],
+  debug:true,
+  devtool:'eval-source-map',
   module: {
     loaders: [{
       test: /\.js$/,
